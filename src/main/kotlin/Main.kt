3cdfed446e615488f0d8899fd8526d3e637f03da
alex mutisya <alexrichamerican@gmail.com>
2023-05-11 15:58:01 +0300

    STRINGS
    STRINGS KOTLIN

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+val numberofcars=100
+    val numberoftracks=25
+    println("i have $numberofcars cars and $numberoftracks tracks")
+
+
+val numberofteachers=12
+    val numberofstudents=42
+    println("their are $numberofteachers teacher and $numberofstudents students")
+
+
+
+val numberofcattles=45
+    val numberofgoats=70
+    println("he bought $numberofcattles cattle and $numberofgoats goats ")
+
+}
